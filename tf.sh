68586e13d82c26d6b91487502d42e7a7c7f7deaf
Abhijit J <abbyjeet@gmail.com>
2022-04-30 23:04:26 +1000

    tf.sh updated comments

--- tf.sh
+++ tf.sh
@@ -4,3 +4,7 @@
 docker run -i -t --rm hashicorp/terraform:latest $@
 
 # echo "Use this command: docker run -i -t --rm hashicorp/terraform:latest [COMMAND]"
+
+# Examples here: 
+# https://learn.hashicorp.com/tutorials/terraform/docker-build?in=terraform/docker-get-started
+# https://audun-nes.medium.com/how-to-use-the-official-terraform-docker-image-2609982114b9
